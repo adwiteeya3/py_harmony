2fa665105b042fc7378ffecdcbf0c5892cebfd05
Adwiteeya <adwiteeya3@gmail.com>
2020-09-13 22:10:47 +0530

    commit_6 - edit and download functionality added

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
+from io import BytesIO
+#import flask_whooshalchemy as wa
+#from forms import SearchForm
+#from wtforms import Form, StringField
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///playlist.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['WHOOSH_BASE'] = 'whoosh'
+
 db = SQLAlchemy(app)
 
 class Songs(db.Model):
+    #__searchable__ = ['title', 'artist', 'album']
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), default='N/A')
     artist = db.Column(db.String(100), default='N/A')
@@ -17,16 +25,18 @@ class Songs(db.Model):
     def __repr__(self):
         return '<Song %r>' % self.title
 
+#wa.whoosh_index(app, Songs)
+
 @app.route('/')
 def index():
     songs = Songs.query.all()
-    return render_template('index.html')
+    return render_template('index.html', songs=songs)
 
 @app.route('/songs', methods=['GET', 'POST'])
 def collection():
     if request.method =='GET':
-        all_songs = Songs.query.all()
-        return render_template('songs.html', songs=all_songs)
+        songs = Songs.query.all()
+        return render_template('songs.html', songs=songs)
     elif request.method == 'POST':
         song_data = request.form
         result = add_song(song_data['title'], song_data['artist'], song_data['year'])
@@ -53,6 +63,29 @@ def delete(id):
     db.session.delete(song)
     db.session.commit()
     return redirect('/songs')
-    
+
+@app.route('/songs/edit/<int:id>', methods=['GET','POST'])
+def edit(id):
+    song = Songs.query.get_or_404(id)
+    if request.method == 'POST':
+        #song.title = request.form['title']
+        song.artist = request.form['artist']
+        song.album = request.form['album']
+        db.session.commit()
+        return redirect('/songs')
+    else:
+        return render_template('edit.html', song= song)
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/songs/download/<int:id>', methods=['GET','POST'])
+def download(id):
+    file_data = Songs.query.filter_by(id=id).first()
+    #file_data.title = request.form['title']
+    #return send_file(BytesIO(file_data.data), attachment_filename=file_data.title, as_attachment=True)
+    return send_file(BytesIO(file_data.data), as_attachment=True, mimetype='audio/mpeg', attachment_filename=file_data.title)
+
 if __name__ == "__main__":
     app.run(debug=True)
