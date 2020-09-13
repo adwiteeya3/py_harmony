3e7271468371b1ef4cca36ccd075dd6c49bacb09
Adwiteeya <adwiteeya3@gmail.com>
2020-09-13 12:54:39 +0530

    commit_4 - upload functionality added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 
@@ -19,12 +19,28 @@ class Songs(db.Model):
 
 @app.route('/')
 def index():
-    #songs = Songs.query.all()
+    songs = Songs.query.all()
     return render_template('index.html')
 
 @app.route('/songs')
 def songs():
     return render_template('songs.html')
+
+@app.route('/upload', methods=['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        files= request.files.getlist('inputFile')
+        for file in files:
+            #song_title = request.form['title']
+            song_artist = request.form['artist']
+            song_album = request.form['album']
+            #file = Songs(title=file.filename, artist=file.filename, album=file.filename, data=file.read())
+            file = Songs(title=file.filename, artist=song_artist, album=song_album, data=file.read())
+            db.session.add(file)
+            db.session.commit()
+        return redirect('/songs')
+    return render_template('upload.html', msg='Please choose a file')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
